952314819ee5571e80c2dc8835f61b1fcb72e41e
Blair Kirkpatrick <6220300+BKirkpatrick@users.noreply.github.com>
2024-04-16 12:01:43 +0100

    my first thoughts on this problem

--- eyring/model.py
+++ eyring/model.py
@@ -1,6 +1,7 @@
 from scipy.constants import Planck, gas_constant, Boltzmann
 from math import exp
 import numpy as np
+import json
 
 gibbs = []  # Gibbs free energy
 kappa = []  # transmission coefficient, [0 -> 1], determines fraction of flux through transition state which leads to product without recrossing transition state.
@@ -13,50 +14,47 @@ def eyring_rate(temperature, gibbs, kappa):
 
 def calculate_gibbs(enthalpy, entropy, temperature):
     """Calculate the Gibbs free energy of system"""
-    gibbs = enthalpy - temperature * entropy
+    gibbs = enthalpy - (temperature * entropy)
     return gibbs
 
-def extract_thermodynamic_parameters(dna_sequence):
+def extract_thermodynamic_parameters(seq, params_dict):
     """Calculate enthalpy and entropy of sequence"""
-    # Thermodynamic parameters for nearest-neighbor pairs
-    parameters = {
-        "AA/TT": {"DeltaH": 7.9, "DeltaS": 22.2},
-        "AT/TA": {"DeltaH": 7.2, "DeltaS": 20.4},
-        "TA/AT": {"DeltaH": 7.2, "DeltaS": 21.3},
-        "CA/GT": {"DeltaH": 8.5, "DeltaS": 22.7},
-        "GT/CA": {"DeltaH": 8.4, "DeltaS": 22.4},
-        "CT/GA": {"DeltaH": 7.8, "DeltaS": 21.0},
-        "GA/CT": {"DeltaH": 8.2, "DeltaS": 22.2},
-        "CG/GC": {"DeltaH": 10.6, "DeltaS": 27.2},
-        "GC/CG": {"DeltaH": 9.8, "DeltaS": 24.4},
-        "GG/CC": {"DeltaH": 8.0, "DeltaS": 19.9}
-    }
-
     # Convert the DNA sequence to its complementary sequence
     complement = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
-    comp_sequence = ''.join([complement[base] for base in dna_sequence])
-
+    seq = seq.replace(" ", "")
     # Initialize total DeltaH and DeltaS
-    total_DeltaH = 0
-    total_DeltaS = 0
-
+    dH = 0
+    dS = 0
     # Iterate through the DNA sequence to get each nearest-neighbor pair
-    for i in range(len(dna_sequence) - 1):
-        pair = dna_sequence[i:i+2]  # Original sequence pair
-        comp_pair = comp_sequence[i:i+2]  # Complementary sequence pair
-        nn_pair = pair + '/' + comp_pair  # Construct the nearest-neighbor key
+    for i in range(len(seq) - 1):
+        pair = seq[i:i+2]  # Original sequence pair
 
-        # Look up the thermodynamic parameters for the current nearest-neighbor pair
-        if nn_pair in parameters:
-            total_DeltaH += 1000 * parameters[nn_pair]["DeltaH"]
-            total_DeltaS += parameters[nn_pair]["DeltaS"]
-        elif nn_pair[::-1] in parameters:
-            total_DeltaH += 1000 * parameters[nn_pair[::-1]]["DeltaH"]
-            total_DeltaS += parameters[nn_pair[::-1]]["DeltaS"]
-        else:
-            print(f"Parameters for {nn_pair} not found.")
+        if pair == "AA" or pair == "TT":
+            key = "AA/TT"
+        elif pair == "AT":
+            key = "AT/AT"
+        elif pair == "TA":
+            key = "TA/TA"
+        elif pair == "CA" or pair == "TG":
+            key = "CA/TG"
+        elif pair == "GT" or pair == "AC":
+            key = "GT/AC"
+        elif pair == "CT" or pair == "AG":
+            key = "CT/AG"
+        elif pair == "GA" or pair == "TC":
+            key = "GA/TC"
+        elif pair == "CG":
+            key = "CG/CG"
+        elif pair == "GC":
+            key = "GC/GC"
+        elif pair == "GG" or pair == "CC":
+            key = "GG/CC"
 
-    return total_DeltaH, total_DeltaS
+    # add contribution from this pair
+        dH += params_dict[key]['dH']
+        dS += params_dict[key]['dS']
+        
+    return dH, dS
 
 def simulate_dna_melting(num_molecules, rate_constant, num_steps):
     # Initialize all molecules as not melted (0)
